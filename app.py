778f8cbe41b26ebbb7ca9ea34a24a62b613748de
alessandromagnani <alessandro.magnani18@studio.unibo.it>
2024-10-01 14:13:18 +0200

    fix: resolve registration form issue

--- app.py
+++ app.py
@@ -1,25 +1,62 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_cors import CORS  # Aggiungi questa importazione
 from dotenv import load_dotenv
 import os
-from routes.auth import auth_bp
+import boto3
+from botocore.exceptions import ClientError
 
 load_dotenv()  # Carica variabili d'ambiente dal file .env
 
 app = Flask(__name__)
 CORS(app)  # Abilita CORS per tutte le rotte
 
-app.config['AWS_REGION'] = os.getenv('AWS_REGION')
-app.config['COGNITO_USER_POOL_ID'] = os.getenv('COGNITO_USER_POOL_ID')
-app.config['AWS_ACCESS_KEY_ID'] = os.getenv('AWS_ACCESS_KEY_ID')
-app.config['AWS_SECRET_ACCESS_KEY'] = os.getenv('AWS_SECRET_ACCESS_KEY')
+# Configura i parametri di AWS Cognito
+AWS_REGION = 'eu-north-1'  # Nome coerente per la regione AWS
+USER_POOL_ID = 'eu-north-1_e5XGPIsEs'
+CLIENT_ID = '2stplsf9hd8d3ks58jlrc2hfth'
 
-# Registra i blueprint
-app.register_blueprint(auth_bp, url_prefix='/api')
+cognito_client = boto3.client('cognito-idp', region_name=AWS_REGION)
 
 @app.route('/')
 def home():
-    return "Welcome to the API"
+    # Restituisci un dizionario con le informazioni
+    return jsonify({
+        "AWS_REGION": AWS_REGION,
+        "COGNITO_USER_POOL_ID": USER_POOL_ID
+    })
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    try:
+        response = cognito_client.sign_up(
+            ClientId=CLIENT_ID,
+            Username=data['email'],
+            Password=data['password'],
+            UserAttributes=[{
+                'Name': 'email',
+                'Value': data['email']
+            }]
+        )
+        return jsonify({"message": "User registered", "response": response}), 200
+    except ClientError as e:
+        return jsonify({"error": str(e)}), 400
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    try:
+        response = cognito_client.initiate_auth(
+            ClientId=CLIENT_ID,
+            AuthFlow='USER_PASSWORD_AUTH',
+            AuthParameters={
+                'USERNAME': data['email'],
+                'PASSWORD': data['password']
+            }
+        )
+        return jsonify({"message": "Login successful", "id_token": response['AuthenticationResult']['IdToken']}), 200
+    except ClientError as e:
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)

2e973c2f17dc32ad0e797a2c5830b3ece24a1c9c
alessandromagnani <alessandro.magnani18@studio.unibo.it>
2025-01-05 22:25:50 +0100

    feat: add AppConfig for centralized configuration management

---
+++ config/app_config.py
@@ -0,0 +1,37 @@
+import os
+
+class AppConfig:
+    # Environment
+    DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+    
+    # LOCALE
+    CORS_ORIGIN = os.environ.get('CORS_ORIGIN', 'http://localhost:8080')
+    
+    # VM
+    # CORS_ORIGIN = os.environ.get('CORS_ORIGIN', 'http://34.122.99.160:8080')
+
+    # Firebase
+    FIREBASE_CRED_PATH = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), 
+        '..', 
+        'config', 
+        'firebase-adminsdk.json'
+    )
+    
+    # Google Cloud Storage
+    GCS_CRED_PATH = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), 
+        '..', 
+        'config', 
+        'meta-geography-438711-r1-de4779cd8c73.json'
+    )
+    GCS_BUCKET_NAME = 'osteoarthritis-portal-archive'
+    
+    # Email
+    SMTP_SERVER = os.environ.get('SMTP_SERVER', 'smtp.gmail.com')
+    SMTP_PORT = int(os.environ.get('SMTP_PORT', 587))
+    SMTP_USERNAME = os.environ.get('SMTP_USERNAME')
+    SMTP_PASSWORD = os.environ.get('SMTP_PASSWORD')
+    
+    # Model
+    MODEL_PATH = 'MODELLO/pesi.h5'
